65e5420104d4c95561bacb29fa77ea4217367fb3
Evgeniy Tsoy <EvTsoy@MacBook.local>
2021-04-12 08:29:47 +0500

    vuex

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <h6 class="text-uppercase text-secondary font-weight-bolder">
-        Check Availability
-        <span v-if="noAvailability" class="text-danger">(NOT AVAILABLE)</span>
-        <span v-if="hasAvailability" class="text-success">(AVAILABLE)</span>
+      Check Availability
+      <span v-if="noAvailability" class="text-danger">(NOT AVAILABLE)</span>
+      <span v-if="hasAvailability" class="text-success">(AVAILABLE)</span>
+    </h6>
 
-        </h6>
     <div class="form-row">
       <div class="form-group col-md-6">
         <label for="from">From</label>
@@ -16,13 +16,9 @@
           placeholder="Start date"
           v-model="from"
           @keyup.enter="check"
-          :class="[{'is-invalid': this.errorFor('from')}]"
+          :class="[{'is-invalid': errorFor('from')}]"
         />
-        <div
-          class="invalid-feedback"
-          v-for="(error, index) in this.errorFor('from')"
-          :key="'from' + index"
-        >{{ error }}</div>
+        <v-errors :errors="errorFor('from')"></v-errors>
       </div>
 
       <div class="form-group col-md-6">
@@ -34,51 +30,52 @@
           placeholder="End date"
           v-model="to"
           @keyup.enter="check"
-          :class="[{'is-invalid': this.errorFor('to')}]"
+          :class="[{'is-invalid': errorFor('to')}]"
         />
-        <div
-          class="invalid-feedback"
-          v-for="(error, index) in this.errorFor('to')"
-          :key="'to' + index"
-        >{{ error }}</div>
+        <v-errors :errors="errorFor('to')"></v-errors>
       </div>
     </div>
 
     <button class="btn btn-secondary btn-block" @click="check" :disabled="loading">Check!</button>
   </div>
 </template>
+
 <script>
+import { is422 } from "./../shared/utils/response";
+import validationErrors from "./../shared/mixins/validationErrors";
+
 export default {
+  mixins: [validationErrors],
+  props: {
+    bookableId: [String, Number]
+  },
   data() {
     return {
       from: null,
       to: null,
       loading: false,
-      status: null,
-      errors: null
+      status: null
     };
   },
   methods: {
     check() {
       this.loading = true;
       this.errors = null;
+
       axios
         .get(
-          `/api/bookables/${this.$route.params.id}/availability?from=${this.from}&to=${this.to}`
+          `/api/bookables/${this.bookableId}/availability?from=${this.from}&to=${this.to}`
         )
         .then(response => {
           this.status = response.status;
         })
         .catch(error => {
-          if (422 === error.response.status) {
+          if (is422(error)) {
             this.errors = error.response.data.errors;
           }
           this.status = error.response.status;
         })
         .then(() => (this.loading = false));
-    },
-    errorFor(field) {
-      return this.hasErrors && this.errors[field] ? this.errors[field] : null;
     }
   },
   computed: {
@@ -94,6 +91,7 @@ export default {
   }
 };
 </script>
+
 <style scoped>
 label {
   font-size: 0.7rem;
@@ -101,10 +99,12 @@ label {
   color: gray;
   font-weight: bolder;
 }
+
 .is-invalid {
   border-color: #b22222;
   background-image: none;
 }
+
 .invalid-feedback {
   color: #b22222;
 }
